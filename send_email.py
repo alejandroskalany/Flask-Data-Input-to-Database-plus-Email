d8e2c179ea919dbcbc2201b5b3ad61754b10ce05
Alejandro Skalany <alskalany@gmail.com>
2021-01-23 18:17:27 -0600

    connected to sql db

---
+++ send_email.py
@@ -0,0 +1,20 @@
+from email.mime.text import MIMEText
+import smtplib
+
+def send_email(email, height, average_height, count):
+    from_email='alskalany@gmail.com'
+    from_password='al9377ex'
+    to_email=email
+
+    subject="Height data"
+    message="Hey there, your height is <strong>%s cm</strong>. Average height of all is <strong>%s cm</strong> and that is calculated out of <strong>%s</strong> people." % (height, average_height, count)
+    msg=MIMEText(message, 'html')
+    msg['Subject']=subject
+    msg['To']=to_email
+    msg['From']=from_email
+
+    gmail=smtplib.SMTP('smtp.gmail.com',587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(from_email, from_password)
+    gmail.send_message(msg)
